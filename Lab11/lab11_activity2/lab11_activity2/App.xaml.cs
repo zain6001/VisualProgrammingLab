9be122de5fc6799c93d7522f729669bdc461c02d
zain6001 <zain756001@gmail.com>
2024-12-12 16:40:42 +0500

    u

---
+++ Lab11/lab11_activity2/lab11_activity2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lab11_activity2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
