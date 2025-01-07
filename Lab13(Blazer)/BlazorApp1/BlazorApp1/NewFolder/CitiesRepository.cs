1c5039132e9a737e563e3805ed4399f72de45291
zain6001 <zain756001@gmail.com>
2025-01-07 22:22:32 +0500

    u

---
+++ Lab13(Blazer)/BlazorApp1/BlazorApp1/NewFolder/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.NewFolder
+{
+    //made by Rayyan
+    public class CitiesRepository
+    {
+        private static List<string> cities =new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
---
+++ Lab13(Blazer)/BlazorApp1/BlazorApp1/NewFolder/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.NewFolder
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomnumber = random.Next(0, 2);
+            IsOnline = randomnumber == 0 ? false : true;
+        }
+        public int Id { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
